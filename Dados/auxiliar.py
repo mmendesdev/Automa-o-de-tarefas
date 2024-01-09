936421f93653260055a053ae6563be51150f03f3
Matheus Mendes <matheus.mendes@somosicev.com>
2024-01-09 20:48:31 -0300

    tarefas

---
+++ Dados/auxiliar.py
@@ -0,0 +1,7 @@
+import pyautogui
+import time
+
+#saber a posição do meu teclado na tela no notebook para ajudar na automção do "bot"
+
+time.sleep(5)
+print(pyautogui.position())
---
+++ Dados/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+pyautogui.press("passo 1")
